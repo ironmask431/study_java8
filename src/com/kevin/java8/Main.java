c70d0d429967f471d7016e7fcb84e8c3c41c0b0c
ironmask431 <ironmask431@gmail.com>
2022-05-25 22:22:52 +0900

    2022.05.25

--- src/com/kevin/java8/Main.java
+++ src/com/kevin/java8/Main.java
@@ -1,9 +1,16 @@
 package com.kevin.java8;
 
+import java.util.function.*;
+
 /**
  * 람다 표현식
  * 함수형 인터페이스의 인스턴스를 만드는 방법으로 쓰일 수 있다.
  * 코드의 길이를 줄일 수 있다.
+ *
+ * 함수형 프로그래밍
+ * 함수를 class object로 사용할 수 있다.
+ * 순수함수 - 사이드 이펙트 만들수없다. 함수밖에있는 값 변경불가 + 외부의값을 참조하지 않는다.
+ * 고차함수 : 함수가 함수를 매개변수로 받을 수 있고, 함수를 리턴할 수도 있다.
  */
 public class Main {
 
@@ -21,5 +28,36 @@ public class Main {
             System.out.println("Hello2");
         };
         runSomething.doIt();
+
+        /**
+         * java8에서 기본제공하는 주요 함수형 인터페이스
+         * Function<T,R>        = T타입 받아서 R 리턴 / R apply(T);
+         * BiFunction<T,U,R>    = 두개의 값 T,U 받아서 R 리턴 / R apply(T,U);
+         * Consumer<T>          = T타입 받아서 리턴X / void Accept(T);
+         * Supplier<T>          = T타입 리턴만. / T get();
+         * Predicate<T>         = T타입 받아서 BOOLEAN 리턴 / boolean test(T)
+         */
+
+        //Function 함수형 인터페이스 예시
+        Function<Integer, Integer> plus10 = (i) -> i+10;
+        System.out.println(plus10.apply(1)); //11
+
+        //BiFunction 함수형 인터페이스 예시
+        BiFunction<Integer, Integer, Integer> test = (i,j) -> i+j;
+        System.out.println(test.apply(1,2)); // 3
+
+        //Consumer 함수형 인터페이스 예시
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(1); //1
+
+        //Supplier 함수형 인터페이스 예시
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get()); //10
+
+        //Predicate 함수형 인터페이스 예시
+        Predicate<Integer> isEven = (i) -> i%2 == 0;
+        System.out.println(isEven.test(4)); //true
+
+
     }
 }
