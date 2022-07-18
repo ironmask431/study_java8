715d89ad267492d8179f8b431b04457d89c9c4df
leesh83 <leesh83@imsmobility.co.kr>
2022-07-18 13:04:24 +0900

    optional 소개

---
+++ src/com/kevin/java8/optional/App.java
@@ -0,0 +1,46 @@
+package com.kevin.java8.optional;
+
+import com.kevin.java8.stream.OnlineClass;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class App {
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api dev", false));
+
+        OnlineClass spring_boot = new OnlineClass(1,"spring boot", true);
+
+//        Progress progress = spring_boot.getProgress();
+//        System.out.println(progress.getStudyDuration()); // 이상태에선 .getProgress() 에서 npe 발생
+
+        //npe를 방지하기 위해 기존 에는 아래와 같이 if문으로 null 체크를 했다.
+        //하지만 이런방식은 에러유발 위험이 큼. null체크를 빼먹을 수 있기때문에.
+//        if(spring_boot.getProgress() != null){
+//            Progress progress2 = spring_boot.getProgress();
+//            System.out.println(progress2.getStudyDuration());
+//        }
+
+        //Optional 타입을 사용해 null 체크 없이 아래와 같이 사용가능
+        Optional<Progress> progress = spring_boot.getProgress();
+        progress.ifPresent(duration -> System.out.println(duration));
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/com/kevin/java8/stream/OnlineClass.java
+++ src/com/kevin/java8/stream/OnlineClass.java
@@ -1,9 +1,15 @@
 package com.kevin.java8.stream;
 
+import com.kevin.java8.optional.Progress;
+
+import java.util.Optional;
+
 public class OnlineClass {
     private Integer id;
     private String title;
     private boolean closed;
+
+    public Progress progress;
 
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
@@ -33,5 +39,20 @@ public class OnlineClass {
 
     public void setClosed(boolean closed) {
         this.closed = closed;
+    }
+
+//    public Progress getProgress() {
+//        return progress;
+//    }
+
+    //Optional 은 여러곳에서 다 사용가능하지만 return 타입으로만 사용하는것이 권장사항.
+    public Optional<Progress> getProgress() {
+        return Optional.ofNullable(progress);
+        //.of() = 안의 객체가 null일수 없다.
+        //.ofNullable() = 안의 객체가 null일수 있다.
+    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
     }
 }
