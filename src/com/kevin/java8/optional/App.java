7a16a60203bf71f1fcf27de80f7c47d5b13cdd47
leesh83 <leesh83@imsmobility.co.kr>
2022-07-18 13:21:27 +0900

    optional 소개

--- src/com/kevin/java8/optional/App.java
+++ src/com/kevin/java8/optional/App.java
@@ -9,13 +9,6 @@ import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
-        List<OnlineClass> springClasses = new ArrayList<>();
-        springClasses.add(new OnlineClass(1, "spring boot", true));
-        springClasses.add(new OnlineClass(2, "spring data jpa", true));
-        springClasses.add(new OnlineClass(3, "spring mvc", false));
-        springClasses.add(new OnlineClass(4, "spring core", false));
-        springClasses.add(new OnlineClass(5, "rest api dev", false));
-
         OnlineClass spring_boot = new OnlineClass(1,"spring boot", true);
 
 //        Progress progress = spring_boot.getProgress();
